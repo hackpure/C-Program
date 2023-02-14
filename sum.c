5edd90095b26b33ce1d41d4dc1310e46affe0b42
Rahul Dixit <125376372+alertproof@users.noreply.github.com>
2023-02-14 21:40:14 +0530

    C Program For Addition Of Two Numbers

---
+++ sum.c
@@ -0,0 +1,12 @@
+// C Program For Addition Of Two Numbers
+
+#include <stdio.h>
+
+void main(){
+	int a, b;
+	printf("Enter Your First Number : ");
+	scanf("%d", &a);
+	printf("Enter Your Second Number : ");
+	scanf("%d", &b);
+	printf("Sum Of %d and %d Is = %d", a, b, a + b);
+}
