9d3dfa7451102e119c1a3b801fab53b1fbe14159
Rahul Dixit <125376372+hackpure@users.noreply.github.com>
2023-02-15 02:27:05 +0530

    C Program To Convert Days To Years, Weeks and Days

--- daystoyears.c
+++ daystoyears.c
@@ -10,34 +10,3 @@ void main(){
 	printf("Number Of Weeks : %d\n", days / 7);
 	printf("Number Of Days : %d\n", days);
 }
-
-
-
-
-/*
-
-If-else Approach
-
-int main(){
-	int days;
-	int years = 0;
-	int weeks = 0;
-	int days_left = 0;
-	printf("Enter Number Of Days : ");
-	scanf("%d", &days);
-	while (days >= 365){
-		days -= 365;
-		years += 1;
-	}
-	while (days >= 7){
-		days -= 7;
-		weeks += 1;
-	}
-	if (days < 7){
-		days_left = days;
-	}
-	printf("Number Of Years : %d", years);
-	printf("\nNumber Of Weeks : %d", weeks);
-	printf("\nNumber Of Days : %d", days_left);
-	return 0;
-} */
